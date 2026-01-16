a18a0f8310afa9b33b811c30bb80e7e5d574863c
Marcellus Siegburg <531939+marcellussiegburg@users.noreply.github.com>
2026-01-16 15:30:55 +0100

    Split into separate modules

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,11 @@
 {-# LANGUAGE LexicalNegation #-}
-{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
-import Data.List (delete)
-import Data.Tuple.Extra (first, second)
 import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game
-
-data World = World
-  { position :: (Float, Float),
-    pressedKeys :: [SpecialKey]
-  }
+import Model
+import View
+import Controller
 
 main :: IO ()
 main =
@@ -23,31 +17,3 @@ main =
     render
     handleInput
     update
-
-initialWorld :: World
-initialWorld = World (0, 0) []
-
-render :: World -> Picture
-render World{..} =
-  uncurry translate position
-    $ color black
-    $ circleSolid 25
-
-handleInput :: Event -> World -> World
-handleInput event world@World{..} =
-  case event of
-    EventKey (SpecialKey k) action _ _ ->
-      world
-        { pressedKeys = case action of
-            Down -> k : pressedKeys
-            Up -> delete k pressedKeys
-        }
-    _ -> world
-
-update :: Float -> World -> World
-update _ world@World{..}
-  | KeyDown `elem` pressedKeys = world{position = second (- 5) position}
-  | KeyUp `elem` pressedKeys = world{position = second (+ 5) position}
-  | KeyLeft `elem` pressedKeys = world{position = first (- 5) position}
-  | KeyRight `elem` pressedKeys = world{position = first (+ 5) position}
-  | otherwise = world
