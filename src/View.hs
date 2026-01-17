dcfc703a1aa29867a94eb5ba2bebf400c8b521c2
Marcellus Siegburg <531939+marcellussiegburg@users.noreply.github.com>
2026-01-17 12:51:48 +0100

    Fix layout

--- src/View.hs
+++ src/View.hs
@@ -12,18 +12,27 @@ import Graphics.Gloss (
   translate,
  )
 
-import Model (Assets (Assets, player), Object (Object, position), World (World, assets, character))
+import Model (
+  Assets (Assets, player),
+  Object (Object, position),
+  World (World, assets, character),
+ )
 
 render :: World -> Picture
-render World {character = Object {position = (x, y)}, assets = Assets {player = playerSprite, ..}, ..} =
-  pictures
-    $
-    -- player sprite
-    playerSprite
-      :
-      -- other stuff in the scene
-      map
-        (translate (-x) (-y))
-        [ translate 80 40 $ circleSolid 30,
-          translate (-250) 0 $ rectangleSolid 100 1000
-        ]
+render
+  World
+    { character = Object {position = (x, y)},
+      assets = Assets {player = playerSprite, ..},
+      ..
+    } =
+    pictures
+      $
+      -- player sprite
+      playerSprite
+        :
+        -- other stuff in the scene
+        map
+          (translate (-x) (-y))
+          [ translate 80 40 $ circleSolid 30,
+            translate (-250) 0 $ rectangleSolid 100 1000
+          ]
